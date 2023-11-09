b4f64769e6ac8684940a403e53099dd4c7dbe5cc
Ank206 <akapoor1_be22@thapar.edu>
2023-11-09 20:10:38 +0530

    Home Section Done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,35 @@ import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
-  return <div className="bg-[#0a192f] w-full h-screen"></div>;
+  return (
+    <div className="bg-[#0a192f] w-full h-screen">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600 text-2xl">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Anush Kapoor
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Full Stack Developer
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px] text-xl ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea vero
+          molestiae eligendi ducimus culpa architecto temporibus sunt officia
+          cupiditate quia quasi, fugit doloremque repudiandae repellat
+          voluptatum error eius maiores ipsa consectetur placeat cum! Hic, culpa
+          ullam nam architecto doloribus quisquam?
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 duration-200">
+            View Work
+            <span className=" flex items-center group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-2" />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
