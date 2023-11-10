b86c70882987b50020f8c49c0fe070219f78b200
Ank206 <akapoor1_be22@thapar.edu>
2023-11-10 17:38:19 +0530

    Skills Done, just the GRID col for response.design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import About from "./components/About.jsx";
 import Home from "./components/Home.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Skills from "./components/Skills.jsx";
 function App() {
   return (
     <div className="">
       <Navbar />
       <Home />
       <About />
+      <Skills />
     </div>
   );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,19 +5,74 @@ import JS from "../assets/javascript(1).png";
 import NODE from "../assets/node.png";
 import ReACT from "../assets/react(1).png";
 import TAILWIND from "../assets/tailwind.png";
+import Github from "../assets/github(1).png";
 
 const Skills = () => {
   return (
-    <div name="skills">
+    <div
+      name="skills"
+      className="bg-[#0a192f] text-gray-300 text-[015x] h-screen  w-full"
+    >
       {/* Container */}
-      <div>
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <p>Experience</p>
-          <p>// These are the technologies I've worked with</p>
+          <p className="text-4xl font-bold  text-gray-300 border-b-4 inline border-pink-600 ">
+            Skills
+          </p>
+          <p className="py-4 text-xl">
+            // These are the technologies I've worked with
+          </p>
         </div>
-        <div>
-          <div>
-            <img src={HTML} alt="HTML icon" />
+        {/* The Below DIV tag is going to be the container for all the icons. */}
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div
+            name="icon"
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+          >
+            <img src={HTML} alt="HTML icon" className="w-20 mx-auto" />
+            <p className="my-4">HTML</p>
+          </div>
+          <div
+            name="icon"
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+          >
+            <img src={CSS} alt="CSS icon" className="w-20 mx-auto" />
+            <p className="my-4">CSS</p>
+          </div>
+          <div
+            name="icon"
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+          >
+            <img src={JS} alt="JS icon" className="w-20 mx-auto" />
+            <p className="my-4">JavaScript</p>
+          </div>
+          <div
+            name="icon"
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+          >
+            <img src={TAILWIND} alt="TAILWIND icon" className="w-20 mx-auto" />
+            <p className="my-4">Tailwind</p>
+          </div>
+          <div
+            name="icon"
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+          >
+            <img src={ReACT} alt="ReACT icon" className="w-20 mx-auto" />
+            <p className="my-4">React</p>
+          </div>
+          <div
+            name="icon"
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+          >
+            <img src={NODE} alt="NODE icon" className="w-20 mx-auto" />
+            <p className="my-4">Node</p>
+          </div>
+          <div
+            name="icon"
+            className="shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+          >
+            <img src={Github} alt="Github icon" className="w-20 mx-auto" />
+            <p className="my-4">Github</p>
           </div>
         </div>
       </div>
