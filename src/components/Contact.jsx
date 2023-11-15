123303cc07bfbf8e9fe51c6802991f5ba4622f5d
Ank206 <akapoor1_be22@thapar.edu>
2023-11-16 01:34:30 +0530

    POST using GETFORMS.IO

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,11 @@ const Contact = () => {
       name="contact"
       className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
     >
-      <form action="" className="flex flex-col max-w-[600px] w-full">
+      <form
+        method="POST"
+        action="https://getform.io/f/63f2d67a-206d-47d9-b20f-bd668a0b1a5f"
+        className="flex flex-col max-w-[600px] w-full"
+      >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
             Contact
